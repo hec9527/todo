ddbf110622d225e8b150188c183fb4bea2e25e06
hec9527 <hec9527@foxmail.com>
2021-08-24 14:42:58 +0800

    抽离todo-items

---
+++ src/components/todo-item/index.tsx
@@ -0,0 +1,100 @@
+import React, { useState, useRef } from 'react';
+import * as Types from '../../index.d';
+import './index.less';
+
+export interface TodoItemProps {
+  isScroll: boolean;
+  todo: Types.TodoItem;
+  changeStatus: (id: number) => void;
+  delTodo: (id: number) => void;
+}
+
+const TodoItem: React.FC<TodoItemProps> = ({ todo: { status, title, id }, changeStatus, delTodo, isScroll }) => {
+  const [left, setLeft] = useState(0);
+  const isMoving = useRef(false);
+  const x = useRef(0);
+
+  const handleMouseDown: React.MouseEventHandler<HTMLDivElement> = (e) => {
+    e.stopPropagation();
+    x.current = e.clientX;
+  };
+
+  const handleMouseMove: React.MouseEventHandler<HTMLDivElement> = (e) => {
+    if (x.current === 0) return;
+    e.stopPropagation();
+    setLeft(e.clientX - x.current);
+    isMoving.current = true;
+  };
+
+  const handleMouseUp: React.DragEventHandler<HTMLDivElement> = (e) => {
+    setTimeout(() => (isMoving.current = false), 0);
+    x.current = 0;
+    if (Math.abs(left) > 100) {
+      setLeft(left > 0 ? 420 : -420);
+      setTimeout(() => {
+        setLeft(0);
+        delTodo(id);
+      }, 100);
+    } else {
+      setLeft(0);
+    }
+  };
+
+  const handleTouchStart: React.TouchEventHandler<HTMLDivElement> = (e) => {
+    e.stopPropagation();
+    if (e.touches.length > 1) return;
+    x.current = e.touches[0].clientX;
+  };
+
+  const handleTouchMove: React.TouchEventHandler<HTMLDivElement> = (e) => {
+    if (isScroll) return;
+    e.stopPropagation();
+    if (e.touches.length > 1) return;
+    const _x = e.touches[0].clientX;
+    setLeft(_x - x.current);
+  };
+
+  const handleTouchEnd: React.TouchEventHandler<HTMLDivElement> = (e) => {
+    e.stopPropagation();
+    x.current = 0;
+    if (Math.abs(left) > 100) {
+      setLeft(left > 0 ? 420 : -420);
+      setTimeout(() => {
+        setLeft(0);
+        delTodo(id);
+      }, 300);
+    } else {
+      setLeft(0);
+    }
+  };
+
+  const handleTodoClick = () => {
+    if (isMoving.current) return;
+    changeStatus(id);
+  };
+
+  return (
+    <div
+      className='todo-item-wrap'
+      draggable={false}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+    >
+      <div
+        style={{ left: `${left}px`, filter: `opacity(${1 - Math.abs(left) / 300})` }}
+        className={'todo-item' + (status === 'done' ? ' done' : '')}
+        onClick={handleTodoClick}
+      >
+        <div className='checkBox'></div>
+        <p className='title'>{title}</p>
+      </div>
+    </div>
+  );
+};
+
+export default TodoItem;
